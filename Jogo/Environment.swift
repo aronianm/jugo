3ca0e033ac17f05b90533ad947b8a0762a5a3e78
Michael Aronian Aronian <michaelaronianaronian@Michaels-Air.home>
2024-02-16 15:58:31 -0500

    maybe fix enviroment vars

--- Jogo/Environment.swift
+++ Jogo/Environment.swift
@@ -12,9 +12,9 @@ import Foundation
 enum Environment {
     static var apiBaseURL: String {
         #if isTestFlight
-                return "http://qa.fogo-fitness.com/"
+                return "http://qa.fogo-fitness.com"
         #elseif RELEASE
-            return "http://production.fogo-fitness.com/"
+            return "http://production.fogo-fitness.com"
         #else
                 return "http://michaels-macbook-air.local:3001"
         #endif

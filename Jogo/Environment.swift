bc5304f09b04efcd1f90fa70a6146893f6e6314b
Michael Aronian Aronian <michaelaronianaronian@Michaels-Air.home>
2024-02-18 19:03:51 -0500

    enviroment vars set for API URL

--- Jogo/Environment.swift
+++ Jogo/Environment.swift
@@ -11,12 +11,6 @@ import Foundation
 
 enum Environment {
     static var apiBaseURL: String {
-        #if isTestFlight
-                return "http://qa.fogo-fitness.com"
-        #elseif RELEASE
-            return "http://production.fogo-fitness.com"
-        #else
-                return "http://michaels-macbook-air.local:3001"
-        #endif
+        return ProcessInfo.processInfo.environment["API_BASE_URL"]!
     }
 }

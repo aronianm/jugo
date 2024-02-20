a2162f9693e37e32cbb821b30361ce674b8d5cea
Michael Aronian Aronian <michaelaronianaronian@Michaels-Air.home>
2024-02-19 23:59:14 -0500

    fix env

--- Jogo/Environment.swift
+++ Jogo/Environment.swift
@@ -44,7 +44,7 @@ enum Environment {
         if (configuration.appConfiguration == .Debug) {
             return "http://michaels-macbook-air.local:3001"
         } else if (configuration.appConfiguration == .TestFlight) {
-            return "http://michaels-macbook-air.local:3001"
+            return "http://qa.fogo-fitness.com"
         } else {
             return "http://production.fogo-fitness.com"
         }
